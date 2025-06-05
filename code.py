109b4df31359e1fe743b4346fd9b57145bc40d12
Ryan Green | GreeniusGenius <74023299+GreeniusGenius@users.noreply.github.com>
2025-06-05 22:15:55 +0100

    Added bluetooth code

--- code.py
+++ code.py
@@ -5,22 +5,53 @@ import usb_midi
 import adafruit_midi
 import digitalio
 import analogio
+import adafruit_ble
+from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
+import asyncio
+import countio
+import board
+
+blebtn = digitalio.DigitalInOut(board.GP20)
+blebtn.switch_to_input(pull=digitalio.Pull.UP)
+
+# Use default HID descriptor
+midi_service = adafruit_ble_midi.MIDIService()
+advertisement = ProvideServicesAdvertisement(midi_service)
+# advertisement.appearance = 961
+
+def blerst():
+    try:
+        ble = adafruit_ble.BLERadio()
+        if ble.connected:
+            for c in ble.connections:
+                c.disconnect()
+    except Error as e:
+        print(e)
 
 try:
-    midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=0)
+    midi = adafruit_midi.MIDI(midi_out=(usb_midi.ports[1], midi_service), out_channel=0)
 except Error as e:
     print(e)
 
-soundADC = analogio.AnalogIn(board.ADC0)
-adafruit_midi.start(midi)
-adafruit_midi.note_on(note=12)
+try:
+    soundADC = analogio.AnalogIn(board.ADC0)
+    adafruit_midi.start(midi)
+    adafruit_midi.note_on(note=12) #just set a default note
+except Exception as e:
+    print(e)
 
 def getSound(io):
-    return (io * 3.3) / 16384
+    return (io * 3.3) / 16384 #midi library is 14 bit but adc is converted to 16 bit by library
 while True:
-    io = soundADC.value
-    sound = getSound(io)
-    adafruit_midi.pitch_bend.PitchBend(sound)
-    adafruit_midi.midi_continue
-
-
+    try:
+        if not blebtn.value:
+            blerst() #check if bluetooth pairing button pressed
+        else:
+            pass
+        io = soundADC.value
+        sound = getSound(io)
+        adafruit_midi.pitch_bend.PitchBend(sound)
+        adafruit_midi.midi_continue
+        ble.start_advertising(advertisement)
+    except Exception as e:
+        print(e)
